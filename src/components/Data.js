0cdb35de0456b3a6cb199645c2d1253e2e64e33a
Gajanan <gajanan@techginie.in>
2023-11-30 19:37:46 +0530

    fixes

---
+++ src/components/Data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        question:'What is Computer',
+        option1 : 'Robot',
+        option2 : 'Software',
+        option3 : 'Human',
+        option4 : 'Machine',
+        ans: 4
+    },
+    {
+        question:'What is Mobile',
+        option1 : 'Robot',
+        option2 : ' Device',
+        option3 : 'Human',
+        option4 : 'Software',
+        ans: 2
+    },
+    {
+        question:'What do you do when you are thirsty',
+        option1 : 'Drink water',
+        option2 : 'Dig',
+        option3 : 'Run',
+        option4 : 'None of the above',
+        ans: 1
+    },
+    {
+        question:'What do you do when you are hungry',
+        option1 : 'Look behind',
+        option2 : 'Run',
+        option3 : 'Eat',
+        option4 : 'Feed',
+        ans: 3
+    },
+    {
+        question:'Why we use charger',
+        option1 : 'To Eat',
+        option2 : 'To Run',
+        option3 : 'To Charge Device',
+        option4 : 'Feed',
+        ans: 3
+    }    
+]
